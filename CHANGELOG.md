470cace3ff8b9f993dd8a04293cddb7a686b5ae5
developer03 togu <developer03togu@developer03s-MacBook-Air.local>
2021-12-08 18:50:51 +0700

    hello world

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 0.0.1 - 2021-12-08
 
-* TODO: Describe initial release.
+* A description for people who will use that package or version.
--- lib/hello.dart
+++ lib/hello.dart
@@ -1,7 +1,11 @@
 library hello;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
-class Calculator {
+class Utils {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  static Widget helloWord() => const Text("hello world");
 }
